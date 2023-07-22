94bf71dea4e24f0911d68c7ff530a60d8a99ee35
super <ctori0816@gmail.com>
2023-07-22 16:56:03 +0300

    update

--- api/index.js
+++ api/index.js
@@ -11,20 +11,18 @@ app.get('/api',(req,res)=>{
         Hello. You just deployed serverless express api</h5>`)
 })
 
-app.get("/rtctoken", (req, res) => {
-  // const appID = "afee3e6b07a94b28b4736ff2c5937313";
-  // const appCertificate = "bb4dfb6f65ef48e8af1fdfda24038a03";
-  // const expirationTimeInSeconds = 3600;
-  // const uid = 0;
-  // const role = Agora.RtcRole.PUBLISHER;
-  // const channel = req.body.channel;
-  // const currentTimestamp = Math.floor(Date.now() / 1000);
-  // const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
+app.post('/rtctoken', (req, res) => {
+  const appID = "afee3e6b07a94b28b4736ff2c5937313";
+  const appCertificate = "bb4dfb6f65ef48e8af1fdfda24038a03";
+  const expirationTimeInSeconds = 3600;
+  const uid = 0;
+  const role = Agora.RtcRole.PUBLISHER;
+  const channel = req.body.channel;
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
 
-  // const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, expirationTimestamp);
-  // res.send({ uid, token });
-  res.send(`<h5 style="color:green">
-        rtctoken</h5>`)
+  const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, expirationTimestamp);
+  res.send({ uid, token });
 });
 
 app.listen(3000, () => {
